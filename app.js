ee8f1ea2a056b624ee2082499f6e45253a6bc167
Luis Martinez <code@Luiss-MBP.attlocal.net>
2021-01-27 11:36:43 -0600

    now im able to get data from front end. Added a post route on the main endpoint

--- app.js
+++ app.js
@@ -7,25 +7,34 @@
     Mongoose
 */
 
+//Global Variable for new items
+
+const items = [];
+
 //Request NPM Packages
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/modules/date.js");
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Create Server
-
-app.get('/', function (req, res) {
-  res.render("list");
-});
- 
 
 
 //Serve Static landing page with EJS
+app.get('/', function (req, res) {
+const todayIs = date.getDate();
+res.render("list", {day:todayIs , addedItems:items});
+});
+ 
+app.post("/", (req , res)=>{
+    const item = req.body.newItem;
+    items.push(item);
+    console.log(items);
+    res.redirect("/");
+})
 
-
-
+// Create Server
 app.listen(3000);
