af693b3db3bc17766ea163eaf429a60c3a5872d8
Luis Martinez <code@Luiss-MBP.attlocal.net>
2021-02-03 12:12:54 -0600

    Added MongoDB as a database

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 //jshint esversion:6
 
-/* Need a few packages from NPM     
+/* Need a few NPM packages      
     Express
     Body Parser
     EJS
     Mongoose
 */
 
-//Global Variable for new items
-
-const items = [];
-
 //Request NPM Packages
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/modules/date.js");
+const mongoose = require('mongoose')
 
+//Mongo database connection
+mongoose.connect('mongodb://localhost:27017/todoDB',{useNewParser: true, useUnifiedTopology: true})
+
+//EJS config
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Schema for item Database 
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model('Item', itemsSchema);
 
+const item1 = new Item({
+  name: "Welcome to your todo list!"
+});
+
+const item2 = new Item({
+  name: "Click the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "Hit this to delete an item ----->"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err)=>{
+  if(err){
+    console.log(err);
+  }else{
+    console.log("successfully added item to the collection")
+  }
+});
 
 //Serve Static landing page with EJS
 app.get('/', function (req, res) {
